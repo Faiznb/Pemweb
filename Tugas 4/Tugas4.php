0eaf00ae35aaee21c11a385196d36126857e97bc
faiznb <faiznurbudi@gmail.com>
2023-03-13 12:46:23 +0700

    Tugas

--- Tugas 4/Tugas4.php
+++ Tugas 4/Tugas4.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tugas 4</title>
     <style>
         *{
             margin: 0;
@@ -34,7 +34,6 @@
 </head>
 <body>
 <?php 
-
    $dbServer = 'localhost';
    $dbUser = 'root';
    $dbPass = '';
